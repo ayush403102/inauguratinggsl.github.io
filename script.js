14de8c97965923c1edcf6c67102f8c1fe8b033fa
Ayush Agarwal <ayush403102@gmail.com>
2023-04-12 21:14:40 +0530

    Fix responsive layout issue in index.html and update font styles in styles.css

---
+++ script.js
@@ -0,0 +1,45 @@
+window.onload = function () 
+{
+  var videoWrap = document.getElementById("videoWrap");
+  var venueText = document.getElementById("venueText");
+  var video = document.getElementById("video");
+  var audio = document.getElementById("audio");
+  var ribbonImg = document.getElementById("ribbonImg");
+  var ribbonLeft = document.getElementById("ribbonLeft");
+  var ribbonRight = document.getElementById("ribbonRight");
+
+  ribbonImg.onclick = function () {
+    cutFunc();
+  };
+
+  ribbonLeft.onclick = function () {
+    cutFunc();
+  };
+
+  ribbonRight.onclick = function () {
+    cutFunc();
+  };
+
+  function cutFunc() {
+    audio.play();
+    ribbonImg.style.transform = "translateY(60vh)";
+    ribbonLeft.style.transform = "rotate(90deg)";
+    ribbonRight.style.transform = "rotate(-90deg)";
+    setTimeout(function () {
+      videoWrap.style.opacity = "1";
+      videoWrap.style.filter = "blur(0)";
+      videoWrap.style.height = "165vh";
+      venueText.style.paddingBottom = "-15vh"
+    }, 500);
+    
+
+    if (video.paused){
+        setTimeout(function () {
+          video.play();
+        }, 4000);
+        setTimeout(function () {
+          videoWrap.style.height = "55vh";
+        }, 124000);
+    } 
+  }
+};
